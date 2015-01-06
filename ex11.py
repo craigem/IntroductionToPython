15d293c23efdda4ca169a6dc1efb2073b4b709b3
Craige McWhirter <craige@mcwhirter.com.au>
2015-01-06 14:23:17 +1100

    Started ex11.

---
+++ ex11.py
@@ -0,0 +1,14 @@
+"""Introduction to Python = Exercise 11
+
+Python 2 or Python 3?
+* Use integer division to show whether your Python interpreter is using Python
+2 or Python 3."""
+
+DIVISION_1 = 6 /2
+try DIVISION_1:
+    if integer
+        print "6 divided 2 is: %s, which is not an integer" % DIVISION_1
+        print "You are using Python 2"
+    except
+        print "You are using Python 3"
+print 7 / 2
