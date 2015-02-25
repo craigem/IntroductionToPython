74b7d17409cbbb4d5cb83af6b0e1fa4447987eba
Craige McWhirter <craige@mcwhirter.com.au>
2015-02-26 10:19:18 +1100

    Made some corrections as per pep8 / pylint.

--- ex11.py
+++ ex11.py
@@ -6,7 +6,7 @@ Python 2 or Python 3?
 
 DIVISION_1 = 6 / 4
 try:
-    isNumberint = int(DIVISION_1)
+    ISNUMBERINT = int(DIVISION_1)
     print "6 divided 4 is: %s, which is an integer" % DIVISION_1
     print "You are using Python 2"
 except:
