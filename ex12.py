8124ed1107c3d914d32e8f0420536acb660ac139
Craige McWhirter <craige@mcwhirter.com.au>
2015-02-26 10:32:48 +1100

    Completed ex12.py

---
+++ ex12.py
@@ -0,0 +1,20 @@
+"""Introduction to Python - Exercise 12
+
+Neat Arithmetic
+* Store the results of at least 5 different calculations in separate
+variables. Make sure you use each operation at least once.
+* Print a series of informative statements, such as "The result of the
+calculation 5+7 is 12."
+"""
+
+VAR1 = 2 + 2
+VAR2 = 5 - 4
+VAR3 = 3 * 3
+VAR4 = 10 / 5
+VAR5 = 6 ** 180
+
+print "The addition of 2 with 2 is %s." % VAR1
+print "The subtraction of 4 from 5 is %s." % VAR2
+print "The multiplication of 3 by 3 is %s." % VAR3
+print "The division of 10 by 5 is %s." % VAR4
+print "The exponent of 6 and 180 is %s." % VAR5
