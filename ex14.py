e737e407298adbf4dea66d0a0e066efd1719634d
Craige McWhirter <craige@mcwhirter.com.au>
2015-03-04 08:17:48 +1100

    Completed ex14 First List

---
+++ ex14.py
@@ -0,0 +1,12 @@
+""" Introduction to Python - Exercise 14
+
+First List
+* Store the values 'python', 'c', and 'java' in a list. Print each of these
+values out, using their position in the list.
+"""
+
+LANGUAGES = ['python', 'c', 'java']
+
+print "The language is %s!" % LANGUAGES[0]
+print "The language is %s!" % LANGUAGES[-2]
+print "The language is %s!" % LANGUAGES[2]
