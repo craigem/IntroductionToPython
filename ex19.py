7b574633f530d808fd055340646d527b3f1a7ede
Craige McWhirter <craige@mcwhirter.com.au>
2015-03-04 10:48:33 +1100

    Completed ex19 - First Neat List - Loop

---
+++ ex19.py
@@ -0,0 +1,14 @@
+""" Introduction to Python - Exercise 19
+
+Your First List - Loop
+
+Repeat Your First List, but this time use a loop to print out your message for
+each item in your list. Again, if you came up with different messages for each
+value in your list, decide on one message to repeat for each value in your
+list.
+"""
+
+FRUITS = ['banana', 'apple', 'pomegranate', 'mango']
+
+for fruit in FRUITS:
+    print "One item in my list is a %s." % fruit
