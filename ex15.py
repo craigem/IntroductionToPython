80e7d189e720c6a4e9c99506a7936ce6c451cba6
Craige McWhirter <craige@mcwhirter.com.au>
2015-03-04 08:22:06 +1100

    Completed ex15 - First Neat List

---
+++ ex15.py
@@ -0,0 +1,14 @@
+""" Introduction to Python - Exercise 15
+
+First Neat List
+
+* Store the values 'python', 'c', and 'java' in a list. Print a statement about
+each of these values, using their position in the list.
+* Your statement could simply be, 'A nice programming language is value.'
+"""
+
+LANGUAGES = ['python', 'c', 'java']
+
+print "A nice programming language is %s." % LANGUAGES[0]
+print "A nice programming language is %s." % LANGUAGES[-2]
+print "A nice programming language is %s." % LANGUAGES[2]
