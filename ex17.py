1a97765d8d4a5691f7150459527bc07284627c9d
Craige McWhirter <craige@mcwhirter.com.au>
2015-03-04 10:36:16 +1100

    Completed ex17 - First List - Loop

---
+++ ex17.py
@@ -0,0 +1,13 @@
+""" Introduction to Python - Exercise 17
+
+First List - Loop
+
+* Repeat First List, but this time use a loop to print out each value in the i
+list.
+
+"""
+
+FRUITS = ['banana', 'apple', 'pomegranate', 'mango']
+
+for fruit in FRUITS:
+    print fruit
