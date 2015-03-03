c4d964dcf4cc164a1f17b2cc679cec7c198afe21
Craige McWhirter <craige@mcwhirter.com.au>
2015-03-04 08:27:54 +1100

    Completed ex16 - Your First List

---
+++ ex16.py
@@ -0,0 +1,12 @@
+""" Introduction to Python - Exercise 16
+
+Your First List
+
+* Think of something you can store in a list. Make a list with three or four
+items, and then print a message that includes at least one item from your list.
+Your sentence could be as simple as, "One item in my list is a ____."
+"""
+
+LANGUAGES = ['banana', 'apple', 'pomegranate', 'mango']
+
+print "One item in my list is a %s." % LANGUAGES[2]
