90c88128f6f40948ec8474568455761bff86d2be
Craige McWhirter <craige@mcwhirter.com.au>
2015-06-22 22:55:52 +1000

    Ran pylint and pep8 over ex20

--- ex20.py
+++ ex20.py
@@ -3,7 +3,7 @@ Exercise 20 - Working List
 
 Working List
 
-* Make a list that includes four careers, such as 'programmer' and 'truck driver'.
+* Make a list that includes four careers.
 * Use the list.index() function to find the index of one career in your list.
 * Use the in function to show that this career is in your list.
 * Use the append() function to add a new career to your list.
@@ -12,32 +12,31 @@ Working List
 '''
 
 # Create a list with 4 careers
-CAREERS = [ 'Programmer', 'Truck Driver', 'Comedian', 'Nurse' ]
+CAREERS = ['Programmer', 'Truck Driver', 'Comedian', 'Nurse']
 print "The current list of careers is:\n"
 for CAREER in CAREERS:
-    print(CAREER)
+    print CAREER
 
 # Get the index of a career and use the "in" function to test this career is
 # in the list:
 CAREER = 'Comedian'
 if CAREER in CAREERS:
-    print "\nComedian is in the careers list and has an index of", CAREERS.index( 'Comedian' ), ".\n"
+    print "\nComedian is in the careers list with an index of", CAREERS.index(
+        'Comedian'), ".\n"
 else:
     print "Comedian is not in the careers list.\n"
 
 # Use the append function to add another career:
 CAREERS.append('Concert Flautist')
 print "Appended Concert Flautist to the list of careers:"
-print(CAREERS)
+print CAREERS
 
 # Use the insert function to add a career to the beginning"
 CAREERS.insert(0, 'Garbologist')
 print "\nInserted Garbologist into the list of careers:"
-print(CAREERS), "\n"
+print CAREERS, "\n"
 
 # Use a loop to show all the careers:
 print "The final list of careers is:"
 for CAREER in CAREERS:
-    print(CAREER)
-
-
+    print CAREER
